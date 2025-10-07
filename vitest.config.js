3e8ca033b60c899a94e1b773cb072782909ab597
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2025-10-07 17:10:31 +0200

    ci: ensure tests run across all platforms
    Unfortunately vitest requires us to do extensive "reconfiguration"
    to ensure tests run locally (on your Chrome), but also on CI, with
    explicitly downloaded binaries.

--- vitest.config.js
+++ vitest.config.js
@@ -2,6 +2,10 @@ import { resolve } from 'node:path';
 
 import { defineConfig } from 'vite';
 
+const HEADLESS = !!process.env.HEADLESS;
+
+const TEST_BROWSERS = (process.env.TEST_BROWSERS || 'chrome').split(',');
+
 export default defineConfig({
   test: {
     globals: true,
@@ -10,9 +14,11 @@ export default defineConfig({
     ],
     browser: {
       enabled: true,
-      instances: [
-        { browser: 'chromium' },
-      ]
+      headless: HEADLESS,
+      provider: HEADLESS ? 'playwright' : 'preview',
+      instances: TEST_BROWSERS.map(
+        browser => ({ browser })
+      )
     }
   }
 });
