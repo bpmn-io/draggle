0c51519a9eb8ded52f0d59af91f5973fa5abeecd
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2025-10-07 13:53:29 +0200

    chore: update vitest*
    Ensure we use modern test dependencies, tests run on Ubuntu 24 Desktop.
    
    Move test configuration to dedicated file.

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { resolve } from 'path';
+import { resolve } from 'node:path';
 
 import { defineConfig } from 'vite';
 
@@ -20,18 +20,6 @@ export default defineConfig({
       external: [
         'contra'
       ]
-    }
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: [
-      resolve(__dirname, 'test', 'setup.js')
-    ],
-    browser: {
-      enabled: true,
-      headless: true,
-      name: 'chrome'
     }
   }
 });
